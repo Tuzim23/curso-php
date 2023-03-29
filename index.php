18ac15a5206bc754833441ca505a058fc32e6468
Arthur Morais <arthurdeferro@gmail.com>
2023-03-29 20:53:30 -0300

    git add.

---
+++ index.php
@@ -0,0 +1,23 @@
+<"DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Titulo da aba</title>
+</head>
+<body>
+    <style>
+        h1{
+            color: #ffffff;
+            background-color: black;
+            text-align: center;
+        }
+    </style>
+    <h1 onclick="alert(Olá Mundo JS')">Minha página</h1>
+    <script>
+        document.write("Olá Mundo JS 2")
+    </script>
+    <?php
+    echo "Olá Mundo PHP";
+    ?>        
+</body>
+</html>
