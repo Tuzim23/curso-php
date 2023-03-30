723a75e531354d563b240f7e2d62c1800970df5e
Arthur Morais <arthurdeferro@gmail.com>
2023-03-29 21:54:13 -0300

    formulario php modificado

---
+++ bem-vindo-post.php
@@ -0,0 +1,11 @@
+<?php
+//  exemplo usando o metodo POST.
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$idade = $_POST["idade"];
+
+echo "Bem Vindo ao mundo PHP $nome <br>";
+echo "E-mail registrado $email<br>";
+echo "Idade registrada: $idade";
+
+?>
