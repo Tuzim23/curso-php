5e7427e52d6ea45a5ba2341a901140254c0d2130
Tuzim23 <arthurdeferro@gmail.com>
2023-04-09 21:16:03 -0300

    Manutenção Evolutiva do Formulário

--- crud-mysql/conectar.php
+++ crud-mysql/conectar.php
@@ -1,25 +1,65 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+// função para fazer a conexão com o banco de dados/
+// essa função retorna uma variavel que tem acesso ao BD.
+function conectar(){
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
 
+    $con = new mysqli($servidor, $usuario, $senha, "mydb");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, "mydb");
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    if($con->connect_error){
+        die("Erro :".$con->connect_error);
+    }
+    //echo "Ok ao conectar";
+    return $con;
 }
 
-function incluir($nome, $email){
-  global $conn;
-  $sql = "insert into pessoa(nome, email) VALUES('$nome','$email')";
-    
-  if($conn->query($sql) === TRUE){
-  return "Ok ao gravar!";
-  }else{ 
-  return "Error: $sql".$conn->error;
-  }
+// função para incluir uma nova pessoa na tabela
+function incluir($nome, $email, $cpf){
+    $con = conectar();
+    $sql = "insert into pessoa(nome, email, cpf) values('$nome','$email','$cpf')";
+    if($con->query($sql) === true){
+        return "Ok ao gravar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
 }
+
+// buscar todas as pessoas que estão gravadas no banco
+function listar(){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa";
+    $resultado = $con->query($sql);
+    return $resultado;
+}
+
+function buscar($id){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa where id = $id";
+    $resultado = $con->query($sql);
+    $resultado = $resultado->fetch_assoc();
+    return $resultado;
+}
+
+function alterar($id, $nome, $email, $cpf){
+    $con = conectar();
+    $sql = "update pessoa set nome = '$nome', email = '$email', cpf = '$cpf' where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Atualizar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+function apagar($id){
+    $con = conectar();
+    $sql = "delete from pessoa where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Apagar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
 ?>
--- crud-mysql/form-pessoa.php
+++ crud-mysql/form-pessoa.php
@@ -5,22 +5,82 @@
     <title>Form Pessoa</title>
 </head>
 <body>
-<form action="" method="post"> 
-    <h1>Formulário de Pessoa</h1>
-    Nome: <br>
-    <input type="text" name="nome"><br>
-    E-mail: <br>
-    <input type="text" name="email"><br>
-    <input type="submit" value="Gravar">
-    </form>
-</body>
 <?php
 include 'conectar.php';
-if ($_SERVER["REQUEST_METHOD"]== "POST") {
+$id = $nome = $email = $cpf = "";
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    if (array_key_exists('id',$_GET)){
+        $id = $_GET['id'];
+        $pessoa = buscar($id);
+        $nome = $pessoa['nome'];
+        $email = $pessoa['email'];
+        $cpf = $pessoa['cpf'];
+    }
+    if (array_key_exists('apagar',$_GET)){
+        $apagar = $_GET['apagar'];
+        $msg = apagar($apagar);
+        echo $msg;
+    }
+}
+?>
+<form action="form-pessoa.php" method="post">
+    <input type="hidden" name="id"  value="<?php echo $id; ?>">
+    <h1>Formulário de Pessoa</h1>
+    Nome: <br>
+    <input type="text" name="nome" value="<?php echo $nome; ?>"><br>
+    E-mail: <br>
+    <input type="text" name="email" value="<?php echo $email; ?>"><br>
+    CPF; <br>
+    <input type="text" name="cpf" value="<?php echo $cpf; ?>"><br>
+    <br>
+    <input type="submit" value="Gravar">
+    <a href="form-pessoa.php">
+    <input type="button" value="Novo">
+    </a>
+    <input type="button" value="Apagar" 
+    <?php echo 'onclick="window.location.replace(\'form-pessoa.php?apagar="';
+    echo '$id\')"'; ?>
+    >
+</form>
+<?php
+//  onclick="window.location.replace('form-pessoa.php');"
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    incluir($nome, $email);
-}
-?> 
+    $cpf = $_POST['email'];
 
-</html>
+    $id = $_POST['id'];
+    if($id == ''){
+        $msg = incluir($nome, $email, $cpf);
+    } else {
+        $msg = alterar($id, $nome, $email, $cpf);
+    }
+    
+    echo $msg;
+}
+
+?>
+<br>
+<table border="1">
+    <tr>
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>CPF</th>
+    </tr>
+    <?php
+    $dados = listar();
+    while ($linha = $dados->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$linha['id']."</td>";
+        echo "<td>".$linha['nome']."</td>";
+        echo "<td>".$linha['email']."</td>";
+        echo "<td>".$linha['cpf']."</td>";
+        echo "<td><a href='form-pessoa.php?id=".$linha['id']."'>Editar</a></td>";
+        echo "<td><a href='form-pessoa.php?apagar=".$linha['id']."'>Apagar</a></td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+</body>
+</html> 
